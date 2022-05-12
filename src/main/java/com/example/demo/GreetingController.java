a8ca36d09cfac1eea224ba93b7d99dbff5efcf2a
Ryan <ryan.w.boehm@gmail.com>
2022-05-11 22:16:40 -0500

    update greeting controller

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
+
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
